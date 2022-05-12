36cbfa14ecb5aef598c780b56106fa6d3474b400
Michael S. Engelbrecht <michaele@toxyngames.com>
2022-05-13 00:56:18 +0200

    Added home screen. (Empty Activity).

--- app/src/main/java/com/example/gollect/CreateCollection.java
+++ app/src/main/java/com/example/gollect/CreateCollection.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateCategory extends AppCompatActivity {
+public class CreateCollection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_category);
+        setContentView(R.layout.activity_create_collection);
     }
 }
