8b4a1742e1274e9ab6a1220329b91bb585706822
Michael S. Engelbrecht <michaele@toxyngames.com>
2022-05-13 01:27:08 +0200

    Added home screen. (Empty Activity).

---
+++ app/src/main/java/com/example/gollect/CreateItemPage.java
@@ -0,0 +1,14 @@
+package com.example.gollect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateItemPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_item_page);
+    }
+}
---
+++ app/src/main/java/com/example/gollect/ViewItemPage.java
@@ -0,0 +1,14 @@
+package com.example.gollect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewItemPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_item_page);
+    }
+}
