23078da68cdb9859f7c47ad896a052624667899a
Simon <simonbaumy@gmail.com>
2022-05-12 18:59:53 +0200

    adadad

--- app/src/main/java/com/example/gollect/MainActivity.java
+++ app/src/main/java/com/example/gollect/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.gollect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView login_Create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_Create = (TextView)findViewById(R.id.login_Create);
+        login_Create.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.login_Create:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/gollect/Register.java
+++ app/src/main/java/com/example/gollect/Register.java
@@ -2,12 +2,56 @@ package com.example.gollect;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView registerUser;
+    private EditText username, email, password, passwordConfirm;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser = (Button) findViewById(R.id.register_button);
+        registerUser.setOnClickListener(this);
+
+        username = (EditText) findViewById(R.id.register_username);
+        email= (EditText) findViewById(R.id.register_email);
+        password = (EditText) findViewById(R.id.register_password);
+        passwordConfirm = (EditText) findViewById(R.id.register_passwordConfirm);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.register_button:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+
+
+
     }
 }
