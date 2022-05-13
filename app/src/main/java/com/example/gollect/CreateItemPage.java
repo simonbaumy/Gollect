5178061afd932b276343f3b4e5e78aff9e1a53b2
Simon <simonbaumy@gmail.com>
2022-05-13 17:50:44 +0200

    adadad

--- app/src/main/java/com/example/gollect/CreateItemPage.java
+++ app/src/main/java/com/example/gollect/CreateItemPage.java
@@ -8,11 +8,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Application;
 import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -25,6 +27,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,16 +40,18 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Objects;
 import java.util.UUID;
 
 public class CreateItemPage extends SelectedCollectionPage implements View.OnClickListener {
 
     private TextView createItem;
-    private EditText editTextItemName, editTextItemType, editTextItemDescription,editTextItemDate ;
+    private EditText editTextItemName, editTextItemType, editTextItemDescription, editTextItemDate;
     private ProgressBar progressBar;
     private ImageView imageView;
     private Button takePhoto;
     private Bitmap imageBitmap;
+    private Context mContext;
 
 
     @Override
@@ -60,6 +65,7 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
         editTextItemDate = (EditText) findViewById(R.id.create_item_date);
         imageView = (ImageView) findViewById(R.id.itemPhoto);
 
+        mContext = getApplicationContext();
 
         createItem = (Button) findViewById(R.id.createItem);
         createItem.setOnClickListener(this);
@@ -81,13 +87,14 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                        dispatchTakePictureIntent();
+                dispatchTakePictureIntent();
 
             }
         });
 
 
     }
+
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
@@ -111,7 +118,7 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.createItem:
                 CreateTheItem();
 
@@ -119,7 +126,7 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
         }
     }
 
-    private void CreateTheItem() {
+    private void CreateTheItem()  {
 
         String iName = editTextItemName.getText().toString().trim();
         String iType = editTextItemType.getText().toString().trim();
@@ -127,23 +134,22 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
         String iDate = editTextItemDate.getText().toString().trim();
 
 
-
-        if(iName.isEmpty()){
+        if (iName.isEmpty()) {
             editTextItemName.setError("Collection name is required");
             editTextItemName.requestFocus();
             return;
         }
-        if(iType.isEmpty()){
+        if (iType.isEmpty()) {
             editTextItemType.setError("Collection type is required");
             editTextItemType.requestFocus();
             return;
         }
-        if(iDescription.isEmpty()){
+        if (iDescription.isEmpty()) {
             editTextItemDescription.setError("Collection goal is required");
             editTextItemDescription.requestFocus();
             return;
         }
-        if(iDate.isEmpty()){
+        if (iDate.isEmpty()) {
             editTextItemDate.setError("Collection goal is required");
             editTextItemDate.requestFocus();
             return;
@@ -151,114 +157,49 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
 
 
         String uniqueString = UUID.randomUUID().toString();
-        insertImage(getContentResolver() , imageBitmap, uniqueString, "Gollect");
-        itemCreator(iType, iDescription, iDate, uniqueString);
-        startActivity(new Intent(this, SelectedCollectionPage.class));
+
+        try {
+            saveImage(imageBitmap, uniqueString);
+            itemCreator(iType, iDescription, iDate, uniqueString);
+            startActivity(new Intent(this, SelectedCollectionPage.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
-    /**
-     * A copy of the Android internals  insertImage method, this method populates the
-     * meta data with DATE_ADDED and DATE_TAKEN. This fixes a common problem where media
-     * that is inserted manually gets saved at the end of the gallery (because date is not populated).
-     * @see android.provider.MediaStore.Images.Media#insertImage(ContentResolver, Bitmap, String, String)
-     */
-    public static final String insertImage(ContentResolver cr,
-                                           Bitmap source,
-                                           String title,
-                                           String description) {
 
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, title);
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
-        values.put(MediaStore.Images.Media.DESCRIPTION, description);
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        // Add the date meta data to ensure the image is added at the front of the gallery
-        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
-        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+    private void saveImage(Bitmap bitmap, @NonNull String name) throws IOException {
+        boolean saved;
+        OutputStream fos;
 
-        Uri url = null;
-        String stringUrl = null;    /* value to be returned */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContentResolver resolver = mContext.getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/" + "Gollect");
+            Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            fos = resolver.openOutputStream(imageUri);
+        } else {
+            String imagesDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).toString() + File.separator +  "Gollect";
 
-        try {
-            url = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            File file = new File(imagesDir);
 
-            if (source != null) {
-                OutputStream imageOut = cr.openOutputStream(url);
-                try {
-                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut);
-                } finally {
-                    imageOut.close();
-                }
-
-                long id = ContentUris.parseId(url);
-                // Wait until MINI_KIND thumbnail is generated.
-                Bitmap miniThumb = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
-                // This is for backward compatibility.
-                storeThumbnail(cr, miniThumb, id, 50F, 50F, MediaStore.Images.Thumbnails.MICRO_KIND);
-            } else {
-                cr.delete(url, null, null);
-                url = null;
+            if (!file.exists()) {
+                file.mkdir();
             }
-        } catch (Exception e) {
-            if (url != null) {
-                cr.delete(url, null, null);
-                url = null;
-            }
+
+            File image = new File(imagesDir, name + ".png");
+            fos = new FileOutputStream(image);
+
         }
 
-        if (url != null) {
-            stringUrl = url.toString();
-        }
-
-        return stringUrl;
-    }
-
-    /**
-     * A copy of the Android internals StoreThumbnail method, it used with the insertImage to
-     * populate the android.provider.MediaStore.Images.Media#insertImage with all the correct
-     * meta data. The StoreThumbnail method is private so it must be duplicated here.
-     * @see android.provider.MediaStore.Images.Media (StoreThumbnail private method)
-     */
-    private static final Bitmap storeThumbnail(
-            ContentResolver cr,
-            Bitmap source,
-            long id,
-            float width,
-            float height,
-            int kind) {
-
-        // create the matrix to scale it
-        Matrix matrix = new Matrix();
-
-        float scaleX = width / source.getWidth();
-        float scaleY = height / source.getHeight();
-
-        matrix.setScale(scaleX, scaleY);
-
-        Bitmap thumb = Bitmap.createBitmap(source, 0, 0,
-                source.getWidth(),
-                source.getHeight(), matrix,
-                true
-        );
-
-        ContentValues values = new ContentValues(4);
-        values.put(MediaStore.Images.Thumbnails.KIND,kind);
-        values.put(MediaStore.Images.Thumbnails.IMAGE_ID,(int)id);
-        values.put(MediaStore.Images.Thumbnails.HEIGHT,thumb.getHeight());
-        values.put(MediaStore.Images.Thumbnails.WIDTH,thumb.getWidth());
-
-        Uri url = cr.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
-
-        try {
-            OutputStream thumbOut = cr.openOutputStream(url);
-            thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut);
-            thumbOut.close();
-            return thumb;
-        } catch (FileNotFoundException ex) {
-            return null;
-        } catch (IOException ex) {
-            return null;
-        }
+        saved = bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        fos.flush();
+        fos.close();
     }
 }
