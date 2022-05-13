1402f4ce1329a0643c7817d41597fcecdad102f0
Simon <simonbaumy@gmail.com>
2022-05-13 18:32:20 +0200

    adadad

--- app/src/main/java/com/example/gollect/CreateItemPage.java
+++ app/src/main/java/com/example/gollect/CreateItemPage.java
@@ -179,29 +179,21 @@ public class CreateItemPage extends SelectedCollectionPage implements View.OnCli
     }
 
     public static boolean validateJavaDate(String strDate) {
-        /* Check if date is 'null' */
+
         if (strDate.trim().equals("")) {
             return true;
         }
-        /* Date is not 'null' */
         else {
-            /*
-             * Set preferred date format,
-             * For example MM-dd-yyyy, MM.dd.yyyy,dd.MM.yyyy etc.*/
             SimpleDateFormat sdfrmt = new SimpleDateFormat("yyyy/MM/dd");
             sdfrmt.setLenient(false);
-            /* Create Date object
-             * parse the string into date
-             */
             try {
                 Date javaDate = sdfrmt.parse(strDate);
                 System.out.println(strDate + " is valid date format");
             }
-            /* Date format is invalid */ catch (ParseException e) {
+            catch (ParseException e) {
                 System.out.println(strDate + " is Invalid Date format");
                 return false;
             }
-            /* Return true if date format is valid */
             return true;
         }
     }
