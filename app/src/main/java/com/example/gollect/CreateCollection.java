703953a6f105e395b88f6b6124c1b20c7d0ba35e
Simon <simonbaumy@gmail.com>
2022-05-13 12:55:30 +0200

    adadad

--- app/src/main/java/com/example/gollect/CreateCollection.java
+++ app/src/main/java/com/example/gollect/CreateCollection.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class CreateCollection extends HomeOld  implements View.OnClickListener{
+public class CreateCollection extends HomeOld implements View.OnClickListener{
 
 
-    private TextView CreateItem;
+    private TextView createCollection;
     private EditText editTextCollectionName, editTextCollectionType, editTextCollectionGoal;
     private ProgressBar progressBar;
 
@@ -29,6 +30,8 @@ public class CreateCollection extends HomeOld  implements View.OnClickListener{
         editTextCollectionType = (EditText) findViewById(R.id.CreateCollectionType);
         editTextCollectionGoal = (EditText) findViewById(R.id.CreateCollectionGoal);
 
+        createCollection = (Button) findViewById(R.id.CreateCollection);
+        createCollection.setOnClickListener(this);
 
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
     }
@@ -67,7 +70,7 @@ public class CreateCollection extends HomeOld  implements View.OnClickListener{
         }
 
         CollectionCreator(cName, cGoal, cType);
-        startActivity(new Intent(CreateCollection.this, HomeOld.class));
+        startActivity(new Intent(this, HomeOld.class));
 
     }
 }
--- app/src/main/java/com/example/gollect/HomeOld.java
+++ app/src/main/java/com/example/gollect/HomeOld.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,8 @@ public class HomeOld extends AppCompatActivity implements View.OnClickListener{
 
     private ListView listView;
 
+    private TextView addCollectionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +28,15 @@ public class HomeOld extends AppCompatActivity implements View.OnClickListener{
         setupData();
         setUpList();
        // setUpOnclickListener();
+
+        addCollectionButton = (Button) findViewById(R.id.addCollection);
+        addCollectionButton.setOnClickListener(this);
     }
 
     private void setupData()
     {
-        GollectCollection circle = new GollectCollection("2022 Hits", "Movies", "5");
-        collectionList.add(circle);
 
-
-
+        //saved data
 
     }
 
@@ -67,7 +71,7 @@ public class HomeOld extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.addCollection:
-                startActivity(new Intent(HomeOld.this, CreateCollection.class));
+                startActivity(new Intent(this, CreateCollection.class));
                 break;
         }
     }
