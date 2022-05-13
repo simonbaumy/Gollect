9e2f7c02801945c2423a1d2a97c422aa66c1c178
Simon <simonbaumy@gmail.com>
2022-05-13 11:13:58 +0200

    adadad

--- app/src/main/java/com/example/gollect/CreateCollection.java
+++ app/src/main/java/com/example/gollect/CreateCollection.java
@@ -3,12 +3,31 @@ package com.example.gollect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateCollection extends AppCompatActivity {
+
+
+    private TextView CreateItem;
+    private EditText editTextCollectionName, editTextCollectionType, EditTextCollectionGoal;
+    private ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_collection);
+
+        editTextCollectionName = (EditText) findViewById(R.id.register_username);
+        editTextCollectionType = (EditText) findViewById(R.id.register_email);
+        EditTextCollectionGoal = (EditText) findViewById(R.id.register_password);
+
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
     }
 }
